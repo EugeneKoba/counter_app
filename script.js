5be1d2ee419ee57233e24f9dcc5a737efeb5b86c
Eugene Annan <eugeneannan@Eugenes-MacBook-Pro.local>
2023-01-31 14:19:15 +0000

    Editing counter_app code HTML CSS Javascript
    Edited counter_app files

---
+++ script.js
@@ -0,0 +1,7 @@
+const counterEl = document.querySelector("#number-el")
+const incrementBtn = document.querySelector("#increment-btn")
+const decrementBtn = document.querySelector("#decrement-btn")
+const refreshBtn = document.getElementById("refresh-btn")
+
+
+console.log(counterEl)
