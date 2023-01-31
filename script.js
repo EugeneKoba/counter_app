75a8251b3512ded14dacb53a36ae1716cc847fd2
Eugene Annan <eugeneannan@Eugenes-MacBook-Pro.local>
2023-01-31 19:02:15 +0000

    Finished counter_app code HTML CSS Javascript
    Finished editing counter_app files

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
-const counterEl = document.querySelector("#number-el")
-const incrementBtn = document.querySelector("#increment-btn")
-const decrementBtn = document.querySelector("#decrement-btn")
-const refreshBtn = document.getElementById("refresh-btn")
+// Capturing each button within js
+let counterEl = document.querySelector("#number-el")
+let incrementBtn = document.querySelector("#increment-btn")
+let decrementBtn = document.querySelector("#decrement-btn")
+let refreshBtn = document.getElementById("refresh-btn")
+// Counter will always start at 0
+let currentResult = 0
 
+// Functions
 
-console.log(counterEl)
+incrementBtn.addEventListener("click", function() {
+    currentResult += 1 // adds 1 for every time the increment button is clicked
+    counterEl.innerHTML = currentResult
+})
+
+decrementBtn.addEventListener("click", function() {
+    currentResult -= 1 // takes away 1 for every time the decrement button is clicked
+    counterEl.innerHTML = currentResult
+})
+
+refreshBtn.addEventListener("click", function() {
+    currentResult = 0 // resets the countEl variable to 0 when the refresh button is clicked
+    counterEl.innerHTML = currentResult
+})
+
+// I used an addEventListener to each button, to either increase, decresde or reset the counterEl variable respectively.
